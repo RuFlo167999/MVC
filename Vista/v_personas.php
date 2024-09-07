460a6387dce15f8372557fad1063f402de5d5426
mconcoba <mconcoba20@gmail.com>
2024-09-07 16:41:07 -0600

    se aplico formato al codigo

--- Vista/v_personas.php
+++ Vista/v_personas.php
@@ -1,91 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Personas</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilos.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Personas</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Inicio</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Inicio</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
-<header class="header bg-light py-4">
+  <header class="header bg-light py-4">
     <div class="container">
-        <div class="row justify-content-center text-center">
-            <img src="Logo.png" alt="Logo" class="img-fluid" style="width: 70px; height: 40px;">
-            <h1 class="mt-3">Conexión con MVC</h1>
-        </div>
+      <div class="row justify-content-center text-center">
+        <img src="Logo.png" alt="Logo" class="img-fluid" style="width: 70px; height: 40px;">
+        <h1 class="mt-3">Conexión con MVC</h1>
+      </div>
     </div>
-</header>
+  </header>
 
-<main class="container mt-5">
+  <main class="container mt-5">
     <table class="table table-striped table-hover table-bordered">
-        <thead class="table-dark">
-            <tr>
-                <th>MOMBRE</th>
-                <th>APELLIDO</th>
-                <th>EDAD</th>
-                <th>SEXO</th>
-                <th>RELIGIÓN</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($personas as $persona): ?>
-                <tr>
-                    <td> <?php echo $persona['nombre']; ?> </td>
-                    <td> <?php echo $persona['apellido']; ?> </td>
-                    <td> <?php echo $persona['edad']; ?> </td>
-                    <td> <?php echo $persona['sexo']; ?> </td>
-                    <td> <?php echo $persona['religion']; ?> </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
+      <thead class="table-dark">
+        <tr>
+          <th>MOMBRE</th>
+          <th>APELLIDO</th>
+          <th>EDAD</th>
+          <th>SEXO</th>
+          <th>RELIGIÓN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($personas as $persona): ?>
+          <tr>
+            <td> <?php echo $persona['nombre']; ?> </td>
+            <td> <?php echo $persona['apellido']; ?> </td>
+            <td> <?php echo $persona['edad']; ?> </td>
+            <td> <?php echo $persona['sexo']; ?> </td>
+            <td> <?php echo $persona['religion']; ?> </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
     </table>
-</main>
+  </main>
 
-<footer class="footer bg-light text-center py-3">
+  <footer class="footer bg-light text-center py-3">
     <div class="container">
-        <p class="mb-0">Todos los derechos reservados © <?php echo date('Y'); ?></p>
+      <p class="mb-0">Todos los derechos reservados © <?php echo date('Y'); ?></p>
     </div>
-</footer>
+  </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
